dbe9f28d991531be674987ed37dc8b1f44c45070
moee <moee@github>
2013-08-08 11:37:13 +0200

    Create Basic Structure

---
+++ tests/Traitor/GetSetTest.php
@@ -0,0 +1,61 @@
+<?php 
+
+class GetSetTest
+    extends PHPUnit_Framework_TestCase
+ {
+     
+    /*
+     * @Test 
+     */
+    public function testHasGetter()
+    {
+        $getSet = $this->getObjectForTrait('Traitor\GetSet');
+        $field = $this->getMock('Traitor\GetSet\Field');
+        $field->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('hello'));
+        $field->expects($this->any())
+            ->method('getValue')
+            ->will($this->returnValue('hello'));
+        
+        $getSet->addField($field);
+        
+        $this->assertEquals("hello", $getSet->getHello());
+    }
+
+    /*
+     * @Test 
+     */
+    public function testHasSetter()
+    {
+        $getSet = $this->getObjectForTrait('Traitor\GetSet');
+        $field = $this->getMock('Traitor\GetSet\Field');
+        $field->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('hello'));
+        $field->expects($this->any())
+            ->method('getValue')
+            ->will($this->returnValue('hello'));
+        
+        $getSet->addField($field);
+         
+        $getSet->setHello('hello');
+    }
+
+    /**
+     * @expectedException \BadMethodCallException
+     */
+    public function testCallGibberish()
+    {
+        $getSet = $this->getObjectForTrait('Traitor\GetSet');
+        $field = $this->getMock('Traitor\GetSet\Field');
+        $field->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('hello'));
+        $field->expects($this->any())
+            ->method('getValue')
+            ->will($this->returnValue('hello'));
+        
+        $getSet->f();
+    }
+}
